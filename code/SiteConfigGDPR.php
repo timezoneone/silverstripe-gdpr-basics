b197b8e626bbd123cbc11cf7ff303101aa0f9998
David <david@Davids-Air.NetComm.Wireless>
2018-05-22 19:11:46 +1200

    added Privacy Policy page link to forms

--- code/SiteConfigGDPR.php
+++ code/SiteConfigGDPR.php
@@ -13,44 +13,53 @@ class SiteConfigGDPR extends DataExtension {
         'PrimaryColor' => 'Color'
     );
 
+    private static $has_one = array(
+        'PrivacyPolicyPage' => 'Page'
+    );
+
+
     public function updateCMSFields(FieldList $fields) {
 
         $fields->addFieldToTab("Root", new Tab('GDPR'));
 
         $fields->addFieldsToTab('Root.GDPR', array(
             CheckboxField::create('GDPRIsActive','Is Active'),
-            DisplayLogicWrapper::create(
-                array(
-                    TextField::create('GTMCode','Google Tag Manager ID')
-                        ->setDescription('Google Tag Manager is only used if user agrees to tracking cookies'),
-                    TextField::create('GACode','Google Analytics ID')
-                        ->setDescription('Anonymized Google Analytics is used when visitor has not accepted cookies.'),
-                    ToggleCompositeField::create('CookiePolicy','Cookie Policy', 
-                        array(
-                            HTMLEditorField::create(
-                                'CookieConsentDescription', 
-                                'Cookie Consent Description', 
-                                $this->owner->CookieConsentDescription, 
-                                'gdpr-basic'
-                            )->setRows(4),
-                            TextField::create('CookieConsentAgreeButtonLabel'),
-                            TextField::create('CookieConsentDeclineButtonLabel'),
-                            ColorField::create('PrimaryColor', 'Cookie Notice Color')
-                        )
-                    ),
-                    ToggleCompositeField::create('PrivacyPolicy','Privacy Policy', 
-                        array(
-                            HTMLEditorField::create(
-                                'PrivacyPolicyDescription', 
-                                'Basic Privacy Policy Description', 
-                                $this->owner->PrivacyPolicyDescription, 
-                                'gdpr-basic'
-                            )->setRows(4)->setDescription('A brief description of what you collect data. This will appear on all forms')
-                        )
-                    )
-                )
-            )->displayIf('GDPRIsActive')->isChecked()->end()
-        )
-        );
+            DisplayLogicWrapper::create(array(
+
+                TextField::create('GTMCode','Google Tag Manager ID')
+                    ->setDescription('Google Tag Manager is only used if user agrees to tracking cookies'),
+                TextField::create('GACode','Google Analytics ID')
+                    ->setDescription('Anonymized Google Analytics is used when visitor has not accepted cookies.'),
+
+                ToggleCompositeField::create('CookiePolicy','Cookie Policy', array(
+                    HTMLEditorField::create(
+                        'CookieConsentDescription', 
+                        'Cookie Consent Description', 
+                        $this->owner->CookieConsentDescription, 
+                        'gdpr-basic'
+                    )->setRows(4),
+                    TextField::create('CookieConsentAgreeButtonLabel'),
+                    TextField::create('CookieConsentDeclineButtonLabel'),
+                    ColorField::create('PrimaryColor', 'Cookie Notice Color')
+                )),
+
+                ToggleCompositeField::create('PrivacyPolicy','Privacy Policy', array(
+
+                    DropdownField::create(
+                        'PrivacyPolicyPageID', 
+                        'Privacy Policy Page', 
+                        SiteTree::get()->exclude(array('ClassName'=>'BlogPost', 'ClassName'=>'TOPage'))->map('ID', 'Title')
+                        )->setEmptyString(''),
+                    HTMLEditorField::create(
+                        'PrivacyPolicyDescription', 
+                        'Basic Privacy Policy Description', 
+                        $this->owner->PrivacyPolicyDescription, 
+                        'gdpr-basic'
+                    )->setRows(4)
+                    ->setDescription('A brief description of what you collect data. This will appear on all forms')
+                ))
+
+            ))->displayIf('GDPRIsActive')->isChecked()->end()
+        ));
     }
 }
--- code/UserFormExtensionGDPR.php
+++ code/UserFormExtensionGDPR.php
@@ -5,9 +5,17 @@ class UserFormExtensionGDPR extends Extension {
     public function updateFormFields(&$fields){
 
         $siteConfig = SiteConfig::current_site_config();
-        if ($siteConfig->GDPRIsActive && $siteConfig->PrivacyPolicyDescription) {
-            $fields->push(LiteralField::create('PrivacyMessage',$siteConfig->PrivacyPolicyDescription));
-        }
 
+        if ($siteConfig->GDPRIsActive) {
+
+            if($siteConfig->PrivacyPolicyDescription || $siteConfig->PrivacyPolicyPage){
+
+                $html = $siteConfig->PrivacyPolicyDescription ? $siteConfig->PrivacyPolicyDescription : '';
+                $html .= $siteConfig->PrivacyPolicyPage() ? '<p><a href="'.$siteConfig->PrivacyPolicyPage()->Link().'">'.$siteConfig->PrivacyPolicyPage()->Title.'</a></p>' : '';
+
+                $fields->push(LiteralField::create('PrivacyMessage',$html));
+
+            }
+        }
     }
 }
