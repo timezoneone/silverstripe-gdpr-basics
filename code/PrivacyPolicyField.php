2d1b17f2ea5229b107d792e5cfc83b063dddf8c4
David <david@Davids-Air.NetComm.Wireless>
2018-05-22 20:56:08 +1200

    added the ability to override the default privacy disclosure on any form by adding a Privacy Policy Disclosure field

---
+++ code/PrivacyPolicyField.php
@@ -0,0 +1,9 @@
+<?php
+
+class PrivacyPolicyField extends EditableLiteralField {
+
+    private static $singular_name = 'Privacy Policy Disclosure';
+
+    private static $plural_name = 'Privacy Policy Disclosure';
+
+}
--- code/UserFormExtensionGDPR.php
+++ code/UserFormExtensionGDPR.php
@@ -3,19 +3,22 @@
 class UserFormExtensionGDPR extends Extension {
 
     public function updateFormFields(&$fields){
-
+        
         $siteConfig = SiteConfig::current_site_config();
 
         if ($siteConfig->GDPRIsActive) {
 
-            if($siteConfig->PrivacyPolicyDisclosure || $siteConfig->PrivacyPolicyPage){
+            //don't insert the Privacy Policy disclosure if the form already has its own..
+            $formHasPolicy = (bool)PrivacyPolicyField::get()->filter('ParentID',Controller::curr()->ID)->Count() > 0;
+
+            if(!$formHasPolicy && ($siteConfig->PrivacyPolicyDisclosure || $siteConfig->PrivacyPolicyPage)){
 
                 $desc = $siteConfig->PrivacyPolicyDisclosure;
                 $privacyPage = $siteConfig->PrivacyPolicyPage();
 
                 $html =  $desc ? $desc : '';
 
-                //if link is included in the Disclosure, we don't need to include it again...
+                //if link to privacy policy is not included in the Disclosure, add it...
                 if($privacyPage && !strpos($desc, $privacyPage->Link())){
                     $html .= '<p><a href="'.$privacyPage->Link().'">'.$privacyPage->Title.'</a></p>';
                 }
