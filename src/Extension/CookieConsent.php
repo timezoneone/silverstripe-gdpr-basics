8de65355f3f7c3aadd59bb0e61c071a04845e3fe
Arthur Apanovics <arthur@timezoneone.com>
2021-02-16 15:41:40 +1300

    Extract ternary logic out of array

--- src/Extension/CookieConsent.php
+++ src/Extension/CookieConsent.php
@@ -18,13 +18,43 @@ class CookieConsent extends Extension
     public function onAfterInit()
     {
         if (SiteConfigGDPR::is_enable_for_request()) {
+            $tagManagerId = $this->siteConfig->GTMCode;
+
+            $CookieConsentDescription = $this->siteConfig
+                ->CookieConsentDescription
+                ? addslashes($this->siteConfig->CookieConsentDescription)
+                : 'This website uses cookies';
+
+            $CookieConsentDescriptionJS = str_replace(
+                "\n",
+                '',
+                $this->siteConfig
+                    ->dbObject('CookieConsentDescription')
+                    ->forTemplate()
+                    ? addslashes(
+                    $this->siteConfig
+                        ->dbObject('CookieConsentDescription')
+                        ->forTemplate()
+                )
+                    : 'This website uses cookies'
+            );
+
+            $CookieConsentAgreeButtonLabel = $this->siteConfig
+                ->CookieConsentAgreeButtonLabel
+                ? addslashes($this->siteConfig->CookieConsentAgreeButtonLabel)
+                : 'Accept';
+
+            $CookieConsentDeclineButtonLabel = $this->siteConfig
+                ->CookieConsentDeclineButtonLabel
+                ? addslashes($this->siteConfig->CookieConsentDeclineButtonLabel)
+                : 'Decline';
 
             $cookieConsentPrompt = new ArrayData([
-                'DomainName'=> $_SERVER['HTTP_HOST'],
-                'CookieConsentDescription' => $this->siteConfig->CookieConsentDescription ? addslashes($this->siteConfig->CookieConsentDescription) : 'This website uses cookies',
-                'CookieConsentDescriptionJS' => str_replace("\n", '', $this->siteConfig->dbObject('CookieConsentDescription')->forTemplate() ? addslashes($this->siteConfig->dbObject('CookieConsentDescription')->forTemplate()) : 'This website uses cookies'),
-                'CookieConsentAgreeButtonLabel' => $this->siteConfig->CookieConsentAgreeButtonLabel ? addslashes($this->siteConfig->CookieConsentAgreeButtonLabel) : 'Accept',
-                'CookieConsentDeclineButtonLabel' => $this->siteConfig->CookieConsentDeclineButtonLabel ? addslashes($this->siteConfig->CookieConsentDeclineButtonLabel) : 'Decline'
+                 'DomainName' => $_SERVER['HTTP_HOST'],
+                 'CookieConsentDescription' => $CookieConsentDescription,
+                 'CookieConsentDescriptionJS' => $CookieConsentDescriptionJS,
+                 'CookieConsentAgreeButtonLabel' => $CookieConsentAgreeButtonLabel,
+                 'CookieConsentDeclineButtonLabel' => $CookieConsentDeclineButtonLabel,
             ]);
 
             Requirements::insertHeadTags(
