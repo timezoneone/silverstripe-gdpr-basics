f73af996b20d175b5fa626853b6ce6684abb35ff
Arthur Apanovics <arthur@timezoneone.com>
2021-02-26 13:53:00 +1300

    Tidy up onAfterInit()

--- src/Extension/CookieConsent.php
+++ src/Extension/CookieConsent.php
@@ -10,7 +10,6 @@ use SilverStripe\View\Requirements;
 
 class CookieConsent extends Extension
 {
-
     public function onBeforeInit()
     {
         $this->siteConfig = SiteConfig::current_site_config();
@@ -21,80 +20,21 @@ class CookieConsent extends Extension
         if (SiteConfigGDPR::is_enable_for_request()) {
             $tagManagerId = $this->siteConfig->GTMCode;
 
-            $CookieConsentDescription = $this->siteConfig
-                ->CookieConsentDescription
-                ? addslashes($this->siteConfig->CookieConsentDescription)
-                : 'This website uses cookies';
-
-            $CookieConsentDescriptionJS = str_replace(
-                "\n",
-                '',
-                $this->siteConfig
-                    ->dbObject('CookieConsentDescription')
-                    ->forTemplate()
-                    ? addslashes(
-                    $this->siteConfig
-                        ->dbObject('CookieConsentDescription')
-                        ->forTemplate()
-                )
-                    : 'This website uses cookies'
-            );
-
-            $CookieConsentAgreeButtonLabel = $this->siteConfig
-                ->CookieConsentAgreeButtonLabel
-                ? addslashes($this->siteConfig->CookieConsentAgreeButtonLabel)
-                : 'Accept';
-
-            $CookieConsentDeclineButtonLabel = $this->siteConfig
-                ->CookieConsentDeclineButtonLabel
-                ? addslashes($this->siteConfig->CookieConsentDeclineButtonLabel)
-                : 'Decline';
-
-            $cookieConsentPrompt = new ArrayData([
-                 'DomainName' => $_SERVER['HTTP_HOST'],
-                 'CookieConsentDescription' => $CookieConsentDescription,
-                 'CookieConsentDescriptionJS' => $CookieConsentDescriptionJS,
-                 'CookieConsentAgreeButtonLabel' => $CookieConsentAgreeButtonLabel,
-                 'CookieConsentDeclineButtonLabel' => $CookieConsentDeclineButtonLabel,
-            ]);
-
             Requirements::insertHeadTags(
-                HTML::createTag('script', [
-                    'async' => true,
-                    'src' => "https://www.googletagmanager.com/gtag/js?id={$tagManagerId}",
-                ])
+                $this->renderGoogleTagManagerScriptTag($tagManagerId)
             );
 
             Requirements::insertHeadTags(
-                "<script>
-                    window.ga = {
-                        gaHasFired: false,
-                        gaCode: '{$tagManagerId}',
-                    };
-                    
-                    function waitForAllTheThings(fn) {
-                        var isDocReady = document.attachEvent
-                            ? document.readyState === 'complete'
-                            : document.readyState !== 'loading';
-
-                        if (isDocReady){
-                            fn();
-                        } else {
-                            document.addEventListener('DOMContentLoaded', fn);
-                        }
-                    }
-                </script>"
+                $this->getGoogleAnalyticsLoaderScriptTag($tagManagerId)
             );
 
-            Requirements::customScript(
-                $cookieConsentPrompt->renderWith('cookieConsentPrompt')
-            );
+            Requirements::customScript($this->renderCookieConsentPrompt());
 
-            $colorPalette = new ArrayData([
-                'PrimaryColor' => $this->siteConfig->PrimaryColor ? '#'.$this->siteConfig->PrimaryColor : '#D65922'
-            ]);
-            Requirements::customCSS($colorPalette->renderWith('Style'));
-            Requirements::javascript('timezoneone/silverstripe-gdpr-basics: client/dist/cookie-permission.min.js');
+            Requirements::customCSS($this->renderStyle());
+
+            Requirements::javascript(
+                'timezoneone/silverstripe-gdpr-basics: client/dist/cookie-permission.min.js'
+            );
         }
     }
 
@@ -103,4 +43,89 @@ class CookieConsent extends Extension
         return SiteConfigGDPR::is_enable_for_request();
     }
 
+    private function renderCookieConsentPrompt()
+    {
+        $CookieConsentDescription = $this->siteConfig->CookieConsentDescription
+            ? addslashes($this->siteConfig->CookieConsentDescription)
+            : 'This website uses cookies';
+
+        $CookieConsentDescriptionJS = str_replace(
+            "\n",
+            '',
+            $this->siteConfig
+                ->dbObject('CookieConsentDescription')
+                ->forTemplate()
+                ? addslashes(
+                    $this->siteConfig
+                        ->dbObject('CookieConsentDescription')
+                        ->forTemplate()
+                )
+                : 'This website uses cookies'
+        );
+
+        $CookieConsentAgreeButtonLabel = $this->siteConfig
+            ->CookieConsentAgreeButtonLabel
+            ? addslashes($this->siteConfig->CookieConsentAgreeButtonLabel)
+            : 'Accept';
+
+        $CookieConsentDeclineButtonLabel = $this->siteConfig
+            ->CookieConsentDeclineButtonLabel
+            ? addslashes($this->siteConfig->CookieConsentDeclineButtonLabel)
+            : 'Decline';
+
+        return (new ArrayData([
+            'DomainName' => $_SERVER['HTTP_HOST'],
+            'CookieConsentDescription' => $CookieConsentDescription,
+            'CookieConsentDescriptionJS' => $CookieConsentDescriptionJS,
+            'CookieConsentAgreeButtonLabel' => $CookieConsentAgreeButtonLabel,
+            'CookieConsentDeclineButtonLabel' => $CookieConsentDeclineButtonLabel,
+        ]))->renderWith('cookieConsentPrompt');
+    }
+
+    private function renderStyle()
+    {
+        $primaryColor = $this->siteConfig->PrimaryColor
+            ? "#{$this->siteConfig->PrimaryColor}"
+            : '#D65922';
+
+        return (new ArrayData([
+            'PrimaryColor' => $primaryColor,
+        ]))->renderWith('Style');
+    }
+
+    private function renderGoogleTagManagerScriptTag($tagManagerId)
+    {
+        return HTML::createTag('script', [
+            'async' => true,
+            'src' => "https://www.googletagmanager.com/gtag/js?id={$tagManagerId}",
+        ]);
+    }
+
+    private function getGoogleAnalyticsLoaderScriptTag($tagManagerId)
+    {
+        $content =
+            /** @lang JavaScript */
+            "window.ga = {
+                    gaHasFired: false,
+                    gaCode: '{$tagManagerId}'
+                };
+                
+                function waitForAllTheThings(fn) {
+                    var isDocReady = document.attachEvent
+                        ? document.readyState === 'complete'
+                        : document.readyState !== 'loading';
+
+                    if (isDocReady){
+                        fn();
+                    } else {
+                        document.addEventListener('DOMContentLoaded', fn);
+                    }
+                }";
+
+        return HTML::createTag(
+            'script',
+            ['type' => 'application/javascript'],
+            $content
+        );
+    }
 }
